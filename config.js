a75c8b6dbed4d4a7c7d96e5e33282f61f0258ca5
Gaming-Squawk <ameentechyt@gmail.com>
2024-09-24 12:35:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -13,8 +13,8 @@ function reloadPresence(client, index) {
         .setDetails("HYDROX MUSIC")
         .setState("ENJOY MUSIC EFFORTLESSLY")
         .setStartTimestamp(global.startTime)
-        .setAssetsLargeImage("https://cdn.discordapp.com/attachments/1136290202118279270/1260236974359576638/VID-20240612-WA0013.gif?ex=66ee2b64&is=66ecd9e4&hm=b32784628581a98f5c412fac2b59eb5d0a421885d59f27690643f79ef69aa285&")
-        .setAssetsLargeText(" Listening to 126 servers")
+        .setAssetsLargeImage("https://cdn.discordapp.com/attachments/1136290202118279270/1287750793084276776/VID-20240612-WA0013.gif?ex=66f357a2&is=66f20622&hm=0fa9b86c86466b2266b570832957cb1d52b51a75c921c5cafa214bc8b03c1f94&")
+        .setAssetsLargeText(" Listening to 130 servers")
         .addButton('ADD TO SERVER', "https://discord.com/oauth2/authorize?client_id=1112673469210034256&permissions=36842105936&integration_type=0&scope=bot+applications.commands")
         .addButton('WEBSITE', "https://hydrox-music.rf.gd/");
 
